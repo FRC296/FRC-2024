ab04d4195ec651d51156c205568ce488d90a9455
Marcus Lee <57817415+GodPuffin@users.noreply.github.com>
2024-03-15 17:44:44 -0400

    Update ClimberSubsystem to use DoubleSolenoid

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,23 +1,24 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase{
 
-    Solenoid climberLeft = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
-    Solenoid climberRight = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
+    DoubleSolenoid climberLeft = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    DoubleSolenoid climberRight = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
 
     public void extendClimber() {
-        climberLeft.set(true);
-        climberRight.set(true);
+        climberLeft.set(Value.kForward);
+        climberRight.set(Value.kForward);
     }
 
     public void retractClimber() {
-        climberLeft.set(false);
-        climberRight.set(false);
+        climberLeft.set(Value.kReverse);
+        climberRight.set(Value.kReverse);
     }
 
     public InstantCommand extendClimberCommand = new InstantCommand(() -> {
